05bd60c1450b1f85d1c617ebb052cf4f7493b082
KYH <aorkslrdnjwm@naver.com>
2024-06-25 13:55:56 +0900

    Implement Loading

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ResourceManager : Manager
 {
+    public static int resourceAmount = 0;
+    public static int resourceLoadCompleted = 0;
     public override IEnumerator Initiate()
     {
         yield return base.Initiate();
