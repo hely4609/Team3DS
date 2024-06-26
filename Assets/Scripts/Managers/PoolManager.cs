1912a1db54c72bfdec9043356962622969539e77
KYH <aorkslrdnjwm@naver.com>
2024-06-26 10:33:09 +0900

    making pooling

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -6,15 +6,38 @@ using UnityEngine;
 public class PoolManager : Manager
 { 
     protected Dictionary<ResourceEnum.Prefab, Queue<GameObject>> poolDictionary;
-    public IEnumerator ClaimPool() { yield return null; }
-
+    
     Transform poolContainer;
     public override IEnumerator Initiate() 
     {
         if (poolContainer == null) poolContainer = new GameObject("Pool Container").transform;
         yield return null; 
     }
-    protected void ReadStock() { }
+
+    //                                              어떤 프리팹, 몇 개
+    public IEnumerator ClaimPool(Dictionary<ResourceEnum.Prefab, int> dictionary, int numbersOnAFrame = 7) 
+    { 
+        for(int i = 0;  i < poolDictionary.Count; i++)
+        {
+
+        }
+        yield return null; 
+    }
+
+    protected void ReadStock(ResourceEnum.Prefab target) 
+    { 
+        // 딕셔너리에 키가 있으면
+        if(poolDictionary.TryGetValue(target, out Queue<GameObject> result))
+        {
+
+        }
+        else
+        {
+            // 없으면 큐를 생성해서 키에 넣기
+            Queue<GameObject> queue = new Queue<GameObject>();
+            poolDictionary.Add(target, queue);
+        }
+    }
     public GameObject Instantiate(ResourceEnum.Prefab target) { return default; }
     public GameObject Instantiate(ResourceEnum.Prefab target, Vector3 pos) { return default; }
     public GameObject Instantiate(ResourceEnum.Prefab target, Vector3 pos, Vector3 euler) { return default; }
