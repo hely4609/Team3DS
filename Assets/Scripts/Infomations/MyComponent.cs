4b98076e222ad6393247a1169b6d0044475bcaf1
NyuuuB <kmh1634311@naver.com>
2024-06-26 10:35:14 +0900

    0626 Character MyComponent붙임

--- Assets/Scripts/Infomations/MyComponent.cs
+++ Assets/Scripts/Infomations/MyComponent.cs
@@ -9,6 +9,16 @@ public class MyComponent : MonoBehaviour
     protected virtual void MyUpdate(float deltaTime) {}
     protected virtual void MyDestroy() {}
 
-    protected void OnEnable() { }
-    protected void OnDisable() { }
+    protected virtual void OnEnable()
+    {
+        GameManager.ObjectStarts += MyStart;
+        GameManager.ObjectUpdates += MyUpdate;
+    }
+    protected virtual void OnDisable()
+    {
+        GameManager.ObjectDestroies -= MyDestroy;
+        GameManager.ObjectDestroies += MyDestroy;
+        GameManager.ObjectUpdates -= MyUpdate;
+        GameManager.ObjectStarts -= MyStart;
+    }
 }
