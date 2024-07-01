8b197493d20044858d74d0240d987b9d2ddbaf51
KYH <aorkslrdnjwm@naver.com>
2024-07-01 16:07:10 +0900

    Implement SetNickname

--- Assets/Scripts/Managers/NetworkManager/NetworkClaim.cs
+++ Assets/Scripts/Managers/NetworkManager/NetworkClaim.cs
@@ -25,10 +25,27 @@ public partial class NetworkManager : Manager
         if(bro.IsSuccess())
         {
             Debug.Log("로그인 성공 " + bro);
+            GameManager.Instance.NetworkManager.currentNetworkState = NetworkState.SignIn;
+            GameManager.Instance.UIManager.Close(UIEnum.SignInCanvas);
         }
         else
         {
             GameManager.Instance.UIManager.ClaimError(bro.GetErrorCode(), bro.GetMessage(), "OK");
+        }
+    }
+
+    public static void UpdateNickname(string inputNickname)
+    {
+        var bro = Backend.BMember.UpdateNickname(inputNickname);
+        if(bro.IsSuccess())
+        {
+            GameManager.Instance.UIManager.Close(UIEnum.SetNicknameCanvas);
+            GameManager.Instance.UIManager.ClaimError("Success", "Nickname has been changed successfully", "OK");
+        }
+        else
+        {
+            GameManager.Instance.UIManager.ClaimError(bro.GetErrorCode(), bro.GetMessage(), "OK");
+
         }
     }
 }
