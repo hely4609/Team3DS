9a15bf2b9d5edbc37766a1fabfa1f89a53173339
KYH <aorkslrdnjwm@naver.com>
2024-07-31 16:04:43 +0900

    hot fix

--- Assets/Scripts/Managers/NetworkManager/NetworkPhotonCallbacks.cs
+++ Assets/Scripts/Managers/NetworkManager/NetworkPhotonCallbacks.cs
@@ -11,7 +11,7 @@ public partial class NetworkPhotonCallbacks : MonoBehaviour, INetworkRunnerCallb
     [SerializeField] private NetworkPrefabRef _playerPrefab;
 
     private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
-    public Dictionary<PlayerRef, NetworkObject> SpawndCharacter => _spawnedCharacters;
+    public Dictionary<PlayerRef, NetworkObject> SpawnedCharacter => _spawnedCharacters;
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
         if (runner.IsServer)
