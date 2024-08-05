cb327f58288cf4c3e69cd9b1ae44d588e1ec8fef
hely4609 <hely4609@naver.com>
2024-08-05 10:59:34 +0900

    프리팹 등록
    에너지 장벽 생성기
    전원 공급기
    육교

--- Assets/Scripts/Infomations/ResourceEnum.cs
+++ Assets/Scripts/Infomations/ResourceEnum.cs
@@ -24,6 +24,9 @@ namespace ResourceEnum
         // Object
         Hammer,
         Rope,
+        PowerSupply,
+        EnergyBarrierGenerator,
+        Bridge,
         // Monster
         EnemyTest
     }
