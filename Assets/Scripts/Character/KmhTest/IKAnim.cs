bff152e93ce431c0c300794c7e3211a09ddb9427
NyuuuB <kmh1634311@naver.com>
2024-08-28 14:06:36 +0900

    IK적용(스크립팅 필요)

---
+++ Assets/Scripts/Character/KmhTest/IKAnim.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKControl : MonoBehaviour
+{
+
+    protected Animator animator;
+
+    public bool ikActive = false;
+    public Transform rightHand = null;
+    public Transform leftHand = null;
+    public Transform rightElbow = null;
+    public Transform leftElbow = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        Debug.Log("이거 매프레임 발동됨?");
+        if (animator)
+        {
+            if (ikActive)
+            {
+
+                // Set the look target position, if one has been assigned
+                //if (lookObj != null)
+                //{
+                //    animator.SetLookAtWeight(1);
+                //    animator.SetLookAtPosition(lookObj.position);
+                //}
+
+                if (rightHand != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
+
+                    animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 1);
+                    animator.SetIKHintPosition(AvatarIKHint.RightElbow, rightElbow.position);
+                }
+
+                if (leftHand != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.rotation);
+
+                    animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 1);
+                    animator.SetIKHintPosition(AvatarIKHint.LeftElbow, leftElbow.position);
+                }
+
+            }
+
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+
+                animator.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 0);
+                animator.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, 0);
+            }
+        }
+    }
+}
