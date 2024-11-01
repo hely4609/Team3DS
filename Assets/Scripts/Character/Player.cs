420d53815e278e4d1f96a71a66e76498e84aedac
KYH <aorkslrdnjwm@naver.com>
2024-11-01 14:59:26 +0900

    fuck

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -2,7 +2,6 @@ using Fusion;
 using System;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Localization.Components;
 using UnityEngine.UI;
