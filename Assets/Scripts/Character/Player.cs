1cc4c2375199057c254cda4fc0a93695e9700f07
KMH <kmh1634311@naver.com>
2025-01-03 15:02:03 +0900

    건물 마우스상하움직임으로 이동가능

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -377,7 +377,7 @@ public partial class Player : Character
                 DesigningBuilding.transform.rotation = Quaternion.Euler(0f, 270f, 0f);
             }
 
-            Vector3 pickPos = transform.position + transform.forward * 5f;
+            Vector3 pickPos = transform.position + transform.forward * Mathf.Lerp(3.5f, 10f, Mathf.InverseLerp(30f + 13f / Mathf.Abs(GroundNormal.y), 0f, Mathf.Clamp(cameraOffset_FPS.localEulerAngles.x > 300f ? 0f : cameraOffset_FPS.localEulerAngles.x, 0f, 30f + 13f / Mathf.Abs(GroundNormal.y))));
             int x = (int)pickPos.x;
             int z = (int)pickPos.z;
             Vector2Int currentPos = new Vector2Int(x, z);
@@ -583,10 +583,13 @@ public partial class Player : Character
             rotate_x += mouseDelta_y;
             rotate_x = Mathf.Clamp(rotate_x, -45f, 30f + 13f / Mathf.Abs(GroundNormal.y));
 
+            
             // 상하회전은 카메라만 회전
             cameraOffset_FPS.localEulerAngles = new Vector3(rotate_x, 0f, 0f);
         }
     }
+
+    
 
     public bool PickUp(GameObject target) { return default; }
     public bool PutDown() { return default; }
